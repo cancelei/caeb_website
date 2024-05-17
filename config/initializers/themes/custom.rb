17f46573b2918efcaa4dfa7f145c8bed9b99cf4b
cancelei <glauber.bannwart@gmail.com>
2024-05-17 11:25:55 -0300

    Update custom.html.erb to improve page layout and styling

--- config/initializers/themes/custom.rb
+++ config/initializers/themes/custom.rb
@@ -1,25 +1,10 @@
-# Theme configuration file
-# ========================
-# This file is used for all theme configuration.
-# It's where you define everything that's editable in Spina CMS.
+# config/initializers/themes.rb
 
 Spina::Theme.register do |theme|
-  # All views are namespaced based on the theme's name
   theme.name = "custom"
   theme.title = "Custom theme"
 
   # Parts
-  # Define all editable parts you want to use in your view templates
-  #
-  # Built-in part types:
-  # - Line
-  # - MultiLine
-  # - Text (Rich text editor)
-  # - Image
-  # - ImageCollection
-  # - Attachment
-  # - Option
-  # - Repeater
   theme.parts = [
     {name: "repeater", title: "Repeater", part_type: "Spina::Parts::Repeater", item_name: "item", parts: %w[line image headline]},
     {name: "line", title: "Line", part_type: "Spina::Parts::Line"},
@@ -31,9 +16,6 @@ Spina::Theme.register do |theme|
   ]
 
   # View templates
-  # Every page has a view template stored in app/views/my_theme/pages/*
-  # You define which parts you want to enable for every view template
-  # by referencing them from the theme.parts configuration above.
   theme.view_templates = [
     {name: "homepage", title: "Homepage", parts: %w[headline body image_collection]},
     {name: "show", title: "Default", parts: %w[body image repeater]},
@@ -41,34 +23,26 @@ Spina::Theme.register do |theme|
   ]
 
   # Custom pages
-  # Some pages should not be created by the user, but generated automatically.
-  # By naming them you can reference them in your code.
   theme.custom_pages = [
-    {name: "homepage", title: "Homepage", deletable: false, view_template: "homepage"},
-    ]
+    {name: "homepage", title: "Homepage", deletable: false, view_template: "homepage"}
+  ]
 
-  # Navigations (optional)
-  # If your project has multiple navigations, it can be useful to configure multiple
-  # navigations.
+  # Navigations
   theme.navigations = [
     {name: "main", label: "Main navigation"}
   ]
 
-  # Layout parts (optional)
-  # You can create global content that doesn't belong to one specific page. We call these layout parts.
-  # You only have to reference the name of the parts you want to have here.
+  # Layout parts
   theme.layout_parts = []
 
-  # Resources (optional)
-  # Think of resources as a collection of pages. They are managed separately in Spina
-  # allowing you to separate these pages from the 'main' collection of pages.
+  # Resources
   theme.resources = [
     {name: "articles", label: "Articles", view_template: "article", slug: "articles"}
   ]
 
-  # Plugins (optional)
+  # Plugins
   theme.plugins = ["reviews", "blog"]
 
-  # Embeds (optional)
+  # Embeds
   theme.embeds = []
 end
