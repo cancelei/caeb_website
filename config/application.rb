aa2ffbc49cfc87a3c9f655c05b1a4dff98f6436e
cancelei <glauber.bannwart@gmail.com>
2024-05-23 17:42:15 -0300

    chore: Update autoload paths for lib and app directories

--- config/application.rb
+++ config/application.rb
@@ -6,15 +6,18 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module CaebWebsite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+    config.autoload_paths += Dir["#{config.root}/lib", "#{config.root}/app/**/*", "#{config.root}/spina-blog/**/"]
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+
 
     # Configuration for the application, engines, and railties goes here.
     #
