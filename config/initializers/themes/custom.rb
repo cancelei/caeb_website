13101d5a20f4ba5893fc46522291b4f691cade5b
cancelei <glauber.bannwart@gmail.com>
2024-05-29 17:18:35 -0300

    Update Gemfile and bin/bundle for improved compatibility with Windows and JRuby platforms

--- config/initializers/themes/custom.rb
+++ config/initializers/themes/custom.rb
@@ -6,30 +6,31 @@ Spina::Theme.register do |theme|
 
   # Parts
   theme.parts = [
-    {name: "repeater", title: "Repeater", part_type: "Spina::Parts::Repeater", item_name: "item", parts: %w[line image headline]},
-    {name: "line", title: "Line", part_type: "Spina::Parts::Line"},
-    {name: "body", title: "Body", hint: "Your content", part_type: "Spina::Parts::Text"},
-    {name: "image_collection", title: "Image collection", part_type: "Spina::Parts::ImageCollection"},
-    {name: "image", title: "Image", part_type: "Spina::Parts::Image"},
-    {name: "headline", title: "Headline", hint: "Used in the header", part_type: "Spina::Parts::Line"},
-    {name: "footer", title: "Footer", part_type: "Spina::Parts::Text"}
+    { name: "repeater", title: "Repeater", part_type: "Spina::Parts::Repeater", item_name: "item",
+      parts: %w[line image headline] },
+    { name: "line", title: "Line", part_type: "Spina::Parts::Line" },
+    { name: "body", title: "Body", hint: "Your content", part_type: "Spina::Parts::Text" },
+    { name: "image_collection", title: "Image collection", part_type: "Spina::Parts::ImageCollection" },
+    { name: "image", title: "Image", part_type: "Spina::Parts::Image" },
+    { name: "headline", title: "Headline", hint: "Used in the header", part_type: "Spina::Parts::Line" },
+    { name: "footer", title: "Footer", part_type: "Spina::Parts::Text" }
   ]
 
   # View templates
   theme.view_templates = [
-    {name: "homepage", title: "Homepage", parts: %w[headline body image_collection]},
-    {name: "show", title: "Default", parts: %w[body image repeater]},
-    {name: "custom", title: "Custom", parts: %w[body image_collection image repeater], exclude_from: %w[articles]}
+    { name: "homepage", title: "Homepage", parts: %w[headline body image_collection] },
+    { name: "show", title: "Default", parts: %w[body image repeater] },
+    { name: "custom", title: "Custom", parts: %w[body image_collection image repeater], exclude_from: %w[articles] }
   ]
 
   # Custom pages
   theme.custom_pages = [
-    {name: "homepage", title: "Homepage", deletable: false, view_template: "homepage"}
+    { name: "homepage", title: "Homepage", deletable: false, view_template: "homepage" }
   ]
 
   # Navigations
   theme.navigations = [
-    {name: "main", label: "Main navigation"}
+    { name: "main", label: "Main navigation" }
   ]
 
   # Layout parts
@@ -37,7 +38,7 @@ Spina::Theme.register do |theme|
 
   # Resources
   theme.resources = [
-    {name: "articles", label: "Articles", view_template: "article", slug: "articles"}
+    { name: "articles", label: "Articles", view_template: "article", slug: "articles" }
   ]
 
   # Plugins
