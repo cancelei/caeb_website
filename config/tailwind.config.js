f8ff5761ea75eac816e02c50c20e78a406a3449f
cancelei <glauber.bannwart@gmail.com>
2024-05-29 17:18:54 -0300

    chore: Add tailwindcss-rails gem and update configuration files

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,16 +1,23 @@
+const spinaBlogConfig = require('../lib/spina_blog/tailwind.config.js');
+
 module.exports = {
   content: [
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js",
     "./app/views/**/*.html.erb",
-    "./app/views/blog_posts/**/*.html.erb",
+    "./app/views/blog_posts/*.html.erb",
     "./app/views/custom/**/*.html.erb",
     "./app/views/devise/**/*.html.erb",
     './app/assets/**/*.css',
+    ...spinaBlogConfig.content,
   ],
   theme: {
     extend: {},
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
 }
